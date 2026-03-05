904284d6ccbc08cdc3966588936d5206ac78b0b6
Kauan-Berti <kauan.g.berti@gmail.com>
2026-03-05 01:43:10 -0300

    feat: initial commit

---
+++ src/main/java/com/group/cinetour/CinetourApiApplication.java
@@ -0,0 +1,13 @@
+package com.group.cinetour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinetourApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinetourApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/cinetour/CinetourApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.cinetour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinetourApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
